ced3eb6d33a6fbcbe2e56d02d7ee71541e46ab1c
mathewCodex <mathewemmanuel212@gmail.com>
2023-08-31 02:42:49 +0000

    Exceptions and Error Handlers in py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """Print an int with "{:d}".format().
+    Args:
+        value (int): The integer to print.
+    Returns:
+        if a TypeError or ValueError occurs - False
+        else - True
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
