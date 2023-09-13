c4f05436b55700e7405fb0ffb911abace0a2ef17
mathewCodex <mathewemmanuel212@gmail.com>
2023-09-13 02:05:23 +0000

    I/O

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,1 @@
-#!/usr/bin/python3
-"""A function that convert a str to a txt"""
-def write_file(filename="", text=""):
-    """Writing a str to a utf-8 text file"""
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""A function that appends a str to the end of UTF8 txt file"""
+"""A function that appends a str to the end of txt file"""
 
 
 
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
+    """appends a string at the end of a text file
+    and returns the number of characters added"""
+    with open(filename, mode="a+", encoding="utf-8") as f:
         return f.write(text)
