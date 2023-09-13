c68178521edff03dfb9dae15af67382116037166
mathewCodex <mathewemmanuel212@gmail.com>
2023-09-13 02:35:20 +0000

    py INPUT/OUTPUT file read write n bytes str

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Def a txt file insertion funct"""
+def append_after(filename="", search_string="", new_string=""):
+    """Insert txt after each line containing a given string"""
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
