46895f4fab5e58a344cfea1e45dd1328c9010717
mathewCodex <mathewemmanuel212@gmail.com>
2023-10-18 14:57:23 +0000

    SQL intro

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 --Create a table called first_table with values id name in my MySQL server
-CREATE TABLE IF NOT EXISTS 'first_table' ('ID' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'first_table' ( id INT, name VARCHAR(256));
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 --Prints the full description of the table in my SQL server
-SHOW CREATE TABLE 'first_table';
+SHOW CREATE TABLE first_table;
