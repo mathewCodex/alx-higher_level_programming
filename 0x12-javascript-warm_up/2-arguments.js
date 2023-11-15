a0b92e04e2e3d5f29c654d7430585f188dd610ee
mathewCodex <mathewemmanuel212@gmail.com>
2023-11-14 20:41:22 -0800

    correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,12 @@
-#!usr/bin/node
+#!/usr/bin/node
 
 const { argv } = require('process');
 const argc = argv.length;
 
-if (argc == 2) 
-{
-	console.log('No argument');
-} else if (argc === 3) 
-{
-	console.log('Argument found');
+if (argc === 2) {
+  console.log('No argument');
+} else if (argc === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
