e5377ba2c861d54b98e856eace7d82e789c12a24
mathewCodex <mathewemmanuel212@gmail.com>
2023-12-01 21:00:07 -0800

    corrected

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Dis the X-re-id header var of a req"""
-
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    print(r.header.get("X-Request-Id"))
+    print(r.headers.get("X-Request-Id"))
