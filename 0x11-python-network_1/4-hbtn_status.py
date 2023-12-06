309e478dcc9815a7a27dab4b56a232f0a0a6aded
mathewCodex <mathewemmanuel212@gmail.com>
2023-12-05 17:54:53 -0800

    fixing bugs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body res:")
+    print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
