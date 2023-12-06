19aa65ff7e0d8f1c7707e79ea3e5ef0c3b1ca4da
mathewCodex <mathewemmanuel212@gmail.com>
2023-12-05 17:47:45 -0800

    fixong error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,17 @@
-#!usr/bin/python3
-"""Send a req to a given URL"""
+#!/usr/bin/python3
+""" Send s a req to a given URL and displays the res body
+"""
 import sys
-import urllib.errror
+import urllib.error
 import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    req = urllib.request.Request(url)
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(req) as res:
-            print(res.read().decode("ascii"))
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
