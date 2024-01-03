bdd14b0f54d379a0bf2a68c090cd1d6d6aaf99aa
mathewCodex <mathewemmanuel212@gmail.com>
2024-01-03 12:59:00 -0800

    indenting

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // using filesytem api
 
-const filesystem = require('fs');
-filesystem.readFile(process.argv[2], 'utf-8',
-	function (err, data) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log(data);
-	});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
